b9baa32d3efa012a53566c1e5e0f854eaf3ec406
MAVADHU <capgemin.avadhut_dalavi@mercedes-benz.com>
2023-12-02 19:24:26 +0530

    added shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="">
       <h1>Main page</h1>
+      <Button variant={"outline"}>Click me ;)</Button>
     </main>
   );
 }
